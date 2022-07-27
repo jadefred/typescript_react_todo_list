60ca988297b7ce1303ad8c31f415941839029af6
jadefred <97032726+jadefred@users.noreply.github.com>
2022-07-27 12:54:48 +0200

    Added interface for task object

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { FC, ChangeEvent, useState } from "react";
 import "./App.css";
+import { ITask } from "./Interfaces";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    }
+    setDeadline(Number(event.target.value));
+  };
+
+
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" placeholder="Task..." name="task" onChange={handleChange} />
+          <input type="number" placeholder="Deadline (in days)..." name="deadline" onChange={handleChange} />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="todoList"></div>
+    </div>
+  );
+};
 
 export default App;
